b342d347db42e33dcd30e566f3d57bbb4182470c
rashed-mahmud2 <mahamud2071@gmail.com>
2025-04-29 08:09:34 +0600

    day-1 done

---
+++ day-1/script.js
@@ -0,0 +1,8 @@
+console.log("Hellow, tapaScript!");
+console.log("Welcome to 40 Days of Javascript!");
+document.write("Check the browser console for a message");
+
+document.getElementById("divId").innerText = "This is Div 1";
+
+document.body.innerHTML +=
+  "This is the modern method that replace document.write()";
